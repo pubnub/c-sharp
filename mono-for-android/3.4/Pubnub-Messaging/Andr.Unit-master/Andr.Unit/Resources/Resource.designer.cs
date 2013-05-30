05439b953e0e1d5d843bcc923ebc1558e28d2b34
crimsonred <crimsonred10@gmail.com>
2013-05-30 23:19:44 +0530

    Core C# changes #5431764503338

--- mono-for-android/3.4/Pubnub-Messaging/Andr.Unit-master/Andr.Unit/Resources/Resource.designer.cs
+++ mono-for-android/3.4/Pubnub-Messaging/Andr.Unit-master/Andr.Unit/Resources/Resource.designer.cs
@@ -19,9 +19,9 @@ namespace Andr.Unit
 	public partial class Resource
 	{
 		
-		Resource()
+		static Resource()
 		{
-			global::Android.Runtime.ResourceIdManager.UpdateIdValues ();
+			global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 		}
 		
 		public static void UpdateIdValues()
@@ -30,6 +30,11 @@ namespace Andr.Unit
 		
 		public partial class Attribute
 		{
+			
+			static Attribute()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
 			
 			private Attribute()
 			{
@@ -42,8 +47,10 @@ namespace Andr.Unit
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
-			// aapt resource value: 0x7f020001
-			public const int monoandroidsplash = 2130837505;
+			static Drawable()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
 			
 			private Drawable()
 			{
@@ -53,35 +60,40 @@ namespace Andr.Unit
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060002
-			public const int dialog_BoolField = 2131099650;
+			// aapt resource value: 0x7f050002
+			public const int dialog_BoolField = 2131034114;
 			
-			// aapt resource value: 0x7f060003
-			public const int dialog_Button = 2131099651;
+			// aapt resource value: 0x7f050003
+			public const int dialog_Button = 2131034115;
 			
-			// aapt resource value: 0x7f060000
-			public const int dialog_LabelField = 2131099648;
+			// aapt resource value: 0x7f050000
+			public const int dialog_LabelField = 2131034112;
 			
-			// aapt resource value: 0x7f060001
-			public const int dialog_LabelSubtextField = 2131099649;
+			// aapt resource value: 0x7f050001
+			public const int dialog_LabelSubtextField = 2131034113;
 			
-			// aapt resource value: 0x7f060005
-			public const int dialog_Panel = 2131099653;
+			// aapt resource value: 0x7f050005
+			public const int dialog_Panel = 2131034117;
 			
-			// aapt resource value: 0x7f060006
-			public const int dialog_RadioButtonList = 2131099654;
+			// aapt resource value: 0x7f050006
+			public const int dialog_RadioButtonList = 2131034118;
 			
-			// aapt resource value: 0x7f060007
-			public const int dialog_Spinner = 2131099655;
+			// aapt resource value: 0x7f050007
+			public const int dialog_Spinner = 2131034119;
 			
-			// aapt resource value: 0x7f060004
-			public const int dialog_ValueField = 2131099652;
+			// aapt resource value: 0x7f050004
+			public const int dialog_ValueField = 2131034116;
 			
-			// aapt resource value: 0x7f060008
-			public const int iFormFieldValue = 2131099656;
+			// aapt resource value: 0x7f050008
+			public const int iFormFieldValue = 2131034120;
 			
-			// aapt resource value: 0x7f060009
-			public const int myButton = 2131099657;
+			// aapt resource value: 0x7f050009
+			public const int myButton = 2131034121;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
 			
 			private Id()
 			{
@@ -145,8 +157,10 @@ namespace Andr.Unit
 			// aapt resource value: 0x7f030011
 			public const int Main = 2130903057;
 			
-			// aapt resource value: 0x7f030012
-			public const int monoandroidsplash = 2130903058;
+			static Layout()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
 			
 			private Layout()
 			{
@@ -162,18 +176,12 @@ namespace Andr.Unit
 			// aapt resource value: 0x7f040000
 			public const int hello = 2130968576;
 			
-			private String()
+			static String()
 			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
 			}
-		}
-		
-		public partial class Style
-		{
 			
-			// aapt resource value: 0x7f050000
-			public const int Mono_Android_Theme_Splash = 2131034112;
-			
-			private Style()
+			private String()
 			{
 			}
 		}
