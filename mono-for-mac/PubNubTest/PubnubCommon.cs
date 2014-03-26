05dcb0cbcd67f794607b3114f0ee2a03bf93ab8f
crimsonred <crimsonred10@gmail.com>
2014-03-26 11:39:29 +0530

    Some updates to JsonFx fix for new presence features

--- mono-for-mac/PubNubTest/PubnubCommon.cs
+++ mono-for-mac/PubNubTest/PubnubCommon.cs
@@ -17,12 +17,9 @@ namespace PubNubMessaging.Tests
         {
             if (PAMEnabled && !EnableStubTest)
                 {
-                //PublishKey = "pub-c-a2650a22-deb1-44f5-aa87-1517049411d5";
-                //  SubscribeKey = "sub-c-a478dd2a-c33d-11e2-883f-02ee2ddab7fe";
-                //  SecretKey = "sec-c-YjFmNzYzMGMtYmI3NC00NzJkLTlkYzYtY2MwMzI4YTJhNDVh";
-                PublishKey = "pub-c-199e0a5c-8aa6-418b-bbca-3e90c20569a8";
-                SubscribeKey = "sub-c-a3d5a1c8-ae97-11e3-a952-02ee2ddab7fe";
-                SecretKey = "sec-c-NGVlNmRkYjAtY2Q1OS00OWM2LWE4NzktNzM5YzIxNGQxZjg3";
+                    PublishKey = "pub-c-a2650a22-deb1-44f5-aa87-1517049411d5";
+                    SubscribeKey = "sub-c-a478dd2a-c33d-11e2-883f-02ee2ddab7fe";
+                    SecretKey = "sec-c-YjFmNzYzMGMtYmI3NC00NzJkLTlkYzYtY2MwMzI4YTJhNDVh";
                 }
         }
     }
