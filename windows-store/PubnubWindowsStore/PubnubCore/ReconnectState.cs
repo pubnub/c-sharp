9b82d3ea686b95d0c9e42837060a4d75dd05cc45
Pandu Masabathula <pandu.masabathula@gmail.com>
2014-07-09 19:36:20 +0530

    windows store sdk

---
+++ windows-store/PubnubWindowsStore/PubnubCore/ReconnectState.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace PubNubMessaging.Core
+{
+	public class ReconnectState<T>
+	{
+		public string[] Channels;
+		public ResponseType Type;
+		public Action<T> Callback;
+		public Action<PubnubClientError> ErrorCallback;
+		public Action<T> ConnectCallback;
+		public object Timetoken;
+
+		public ReconnectState()
+		{
+			Channels = null;
+			Callback = null;
+			ConnectCallback = null;
+			Timetoken = null;
+		}
+	}
+}
+
