c05c14b8dbac2bda994041a7bd51a2fcd1562317
Pandu Masabathula <pandu.masabathula@gmail.com>
2014-08-27 19:07:16 +0530

    wp8 unit tests
    Made fix to ThenPublishKeyShouldNotBeEmpty test case.
    Added code  for auto run of unit tests and logging to Isolated Storage
    File.

--- windows-phone-8/PubnubWindowsPhone.Test/MainPage.xaml.cs
+++ windows-phone-8/PubnubWindowsPhone.Test/MainPage.xaml.cs
@@ -11,6 +11,9 @@ using PubnubWindowsPhone.Test.Resources;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Phone.Testing;
+using Microsoft.Phone.Testing.Harness;
+using System.IO.IsolatedStorage;
+using System.IO;
 
 namespace PubnubWindowsPhone.Test
 {
@@ -24,11 +27,69 @@ namespace PubnubWindowsPhone.Test
 
             if (runUnitTests)
             {
-                Content = UnitTestSystem.CreateTestPage();
+                UnitTestSettings settings = UnitTestSystem.CreateDefaultSettings();
+#if AUTOUNITTEST
+                settings.StartRunImmediately = true;
+                settings.ShowTagExpressionEditor = false;
+                settings.TestHarness.TestHarnessCompleted += TestRunCompletedCallback;
+#endif
+
+
+                Content = UnitTestSystem.CreateTestPage(settings);
+                
             }
 
             // Sample code to localize the ApplicationBar
             //BuildLocalizedApplicationBar();
+        }
+
+        void TestRunCompletedCallback(object sender, TestHarnessCompletedEventArgs e)
+        {
+            int testsPassed = 0;
+            int testsFailed = 0;
+            int testsInconclusive = 0;
+            UnitTestHarness testHarness = sender as UnitTestHarness;
+            using (IsolatedStorageFile isoStore = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                using (IsolatedStorageFileStream stream = isoStore.OpenFile("pubnublog.txt", FileMode.OpenOrCreate))
+                {
+                    using (StreamWriter writer = new StreamWriter(stream))
+                    {
+                        foreach (ScenarioResult result in testHarness.Results)
+                        {
+                            writer.WriteLine(result.ToString());
+                            System.Diagnostics.Debug.WriteLine(result.ToString());
+                            switch (result.Result)
+                            {
+                                case TestOutcome.Passed:
+                                case TestOutcome.Completed:
+                                    testsPassed++;
+                                    break;
+                                case TestOutcome.Inconclusive:
+                                    testsInconclusive++;
+                                    break;
+                                default:
+                                    testsFailed++;
+                                    // must be a failure of some kind
+                                    // perform some outputting
+                                    break;
+                            }
+                        }
+                        writer.WriteLine("Total Tests = " + testHarness.Results.Count);
+                        writer.WriteLine("Tests Passed = " + testsPassed);
+                        writer.WriteLine("Tests Failed = " + testsFailed);
+                        writer.WriteLine("Tests Inconclusive = " + testsInconclusive);
+
+                        System.Diagnostics.Debug.WriteLine("Total Tests = " + testHarness.Results.Count);
+                        System.Diagnostics.Debug.WriteLine("Tests Passed = " + testsPassed);
+                        System.Diagnostics.Debug.WriteLine("Tests Failed = " + testsFailed);
+                        System.Diagnostics.Debug.WriteLine("Tests Inconclusive = " + testsInconclusive);
+
+                        writer.Close();
+                    }
+                    stream.Close();
+                }
+            }
         }
 
         // Sample code for building a localized ApplicationBar
