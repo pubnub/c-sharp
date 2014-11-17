6b4a1d72bd9a72d74f6b057401b6823ae89caed5
Pandu Masabathula <pandu.masabathula@gmail.com>
2014-11-17 19:06:34 +0530

    sl: unit tests + core file update

--- silverlight/PubnubSilverlight.Example/Views/CodeExampleView.xaml.cs
+++ silverlight/PubnubSilverlight.Example/Views/CodeExampleView.xaml.cs
@@ -165,7 +165,7 @@ namespace PubnubSilverlight
                 }
                 else if (publishView.radToastPublish.IsChecked.Value)
                 {
-                    ToastNotification toast = new ToastNotification();
+                    MpnsToastNotification toast = new MpnsToastNotification();
                     //toast.type = "toast";
                     toast.text1 = "hardcode message";
                     Dictionary<string, object> dicToast = new Dictionary<string, object>();
@@ -178,7 +178,7 @@ namespace PubnubSilverlight
                 {
                     pubnub.PushRemoteImageDomainUri.Add(new Uri("http://cdn.flaticon.com"));
 
-                    FlipTileNotification tile = new FlipTileNotification();
+                    MpnsFlipTileNotification tile = new MpnsFlipTileNotification();
                     tile.title = "front title";
                     tile.count = 6;
                     tile.back_title = "back title";
@@ -193,7 +193,7 @@ namespace PubnubSilverlight
                 }
                 else if (publishView.radCycleTilePublish.IsChecked.Value)
                 {
-                    CycleTileNotification tile = new CycleTileNotification();
+                    MpnsCycleTileNotification tile = new MpnsCycleTileNotification();
                     tile.title = "front title";
                     tile.count = 2;
                     tile.images = new string[] { "Assets/Tiles/pubnub1.png", "Assets/Tiles/pubnub2.png", "Assets/Tiles/pubnub3.png", "Assets/Tiles/pubnub4.png" };
@@ -206,7 +206,7 @@ namespace PubnubSilverlight
                 }
                 else if (publishView.radIconicTilePublish.IsChecked.Value)
                 {
-                    IconicTileNotification tile = new IconicTileNotification();
+                    MpnsIconicTileNotification tile = new MpnsIconicTileNotification();
                     tile.title = "front title";
                     tile.count = 2;
                     tile.wide_content_1 = "my wide content";
