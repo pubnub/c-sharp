e15af88015758a01ebc6ca3ddc1af4f4683a3021
Pandu Masabathula <pandu.masabathula@gmail.com>
2014-11-18 00:07:05 +0530

    windows store 8: unit tests + core file update

--- windows-store/windows8/PubnubWindowsStore/PubnubCore/PubnubWin.cs
+++ windows-store/windows8/PubnubWindowsStore/PubnubCore/PubnubWin.cs
@@ -1662,5 +1662,59 @@ namespace PubNubMessaging.Core
 
     #endregion
 
+    #region "MPNS Toast/Tiles"
+    
+    public class MpnsToastNotification
+    {
+        public string type = "toast";
+        public string text1 = "";
+        public string text2 = "";
+        public string param = "";
+    }
+
+    public class MpnsFlipTileNotification
+    {
+        public string type = "flip";
+        public int delay = 0;
+        public string title = "";
+        public int? count = 0;
+        public string small_background_image = "";
+        public string background_image = "";
+        public string back_background_image = "";
+        public string back_content = "";
+        public string back_title = "";
+        public string wide_background_image = "";
+        public string wide_back_background_image = "";
+        public string wide_back_content = "";
+    }
+
+    public class MpnsCycleTileNotification
+    {
+        public string type = "cycle";
+        public int delay = 0;
+        public string title = "";
+        public int? count = 0;
+        public string small_background_image = "";
+        public string[] images = null;
+    }
+
+    public class MpnsIconicTileNotification
+    {
+        public string type = "iconic";
+        public int delay = 0;
+        public string title = "";
+        public int? count = 0;
+        public string icon_image = "";
+        public string small_icon_image = "";
+        public string background_color = "";
+        public string wide_content_1 = "";
+        public string wide_content_2 = "";
+        public string wide_content_3 = "";
+    }
+    
+    #endregion
+
+
+
 }
 
