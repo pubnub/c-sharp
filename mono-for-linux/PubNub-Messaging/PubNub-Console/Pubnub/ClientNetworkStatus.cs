17d5e88d3d541b48d1bd9c32c25197177b698051
Pandu Masabathula <pandu.masabathula@gmail.com>
2015-04-17 09:01:03 +0530

    mono-for-linux: null handling

--- mono-for-linux/PubNub-Messaging/PubNub-Console/Pubnub/ClientNetworkStatus.cs
+++ mono-for-linux/PubNub-Messaging/PubNub-Console/Pubnub/ClientNetworkStatus.cs
@@ -271,8 +271,10 @@ namespace PubNubMessaging.Core
 			PubnubErrorCode errorType = PubnubErrorCodeHelper.GetErrorType(ex);
 			int statusCode = (int)errorType;
 			string errorDescription = PubnubErrorCodeDescription.GetStatusCodeDescription(errorType);
-            PubnubClientError error = new PubnubClientError(statusCode, PubnubErrorSeverity.Warn, true, ex.Message, ex, PubnubMessageSource.Client, null, null, errorDescription, string.Join(",", channels), string.Join(",", channelGroups));
-			GoToCallback(error, errorCallback);
+            string channel = (channels == null) ? "" : string.Join(",", channels);
+            string channelGroup = (channelGroups == null) ? "" : string.Join(",", channelGroups);
+            PubnubClientError error = new PubnubClientError(statusCode, PubnubErrorSeverity.Warn, true, ex.ToString(), ex, PubnubMessageSource.Client, null, null, errorDescription, channel, channelGroup);
+            GoToCallback(error, errorCallback);
 
 			LoggingMethod.WriteToLog(string.Format("DateTime {0} checkInternetStatus Error. {1}", DateTime.Now.ToString(), ex.ToString()), LoggingMethod.LevelError);
 			callback(false);
