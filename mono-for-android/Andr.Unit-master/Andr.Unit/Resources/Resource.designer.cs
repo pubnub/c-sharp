da90828a90904d68fa9b65b67736030aef658f0a
Pandu Masabathula <pandumasabathula@Pandus-MacBook-Pro.local>
2015-04-29 01:06:34 +0530

    mono-for-android: resource files related(auto generated)

--- mono-for-android/Andr.Unit-master/Andr.Unit/Resources/Resource.designer.cs
+++ mono-for-android/Andr.Unit-master/Andr.Unit/Resources/Resource.designer.cs
@@ -48,6 +48,9 @@ namespace Andr.Unit
 			// aapt resource value: 0x7f020000
 			public const int Icon = 2130837504;
 			
+			// aapt resource value: 0x7f020001
+			public const int monoandroidsplash = 2130837505;
+			
 			static Drawable()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
@@ -61,35 +64,35 @@ namespace Andr.Unit
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050002
-			public const int dialog_BoolField = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int dialog_BoolField = 2131099650;
 			
-			// aapt resource value: 0x7f050003
-			public const int dialog_Button = 2131034115;
+			// aapt resource value: 0x7f060003
+			public const int dialog_Button = 2131099651;
 			
-			// aapt resource value: 0x7f050000
-			public const int dialog_LabelField = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int dialog_LabelField = 2131099648;
 			
-			// aapt resource value: 0x7f050001
-			public const int dialog_LabelSubtextField = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int dialog_LabelSubtextField = 2131099649;
 			
-			// aapt resource value: 0x7f050005
-			public const int dialog_Panel = 2131034117;
+			// aapt resource value: 0x7f060005
+			public const int dialog_Panel = 2131099653;
 			
-			// aapt resource value: 0x7f050006
-			public const int dialog_RadioButtonList = 2131034118;
+			// aapt resource value: 0x7f060006
+			public const int dialog_RadioButtonList = 2131099654;
 			
-			// aapt resource value: 0x7f050007
-			public const int dialog_Spinner = 2131034119;
+			// aapt resource value: 0x7f060007
+			public const int dialog_Spinner = 2131099655;
 			
-			// aapt resource value: 0x7f050004
-			public const int dialog_ValueField = 2131034116;
+			// aapt resource value: 0x7f060004
+			public const int dialog_ValueField = 2131099652;
 			
-			// aapt resource value: 0x7f050008
-			public const int iFormFieldValue = 2131034120;
+			// aapt resource value: 0x7f060008
+			public const int iFormFieldValue = 2131099656;
 			
-			// aapt resource value: 0x7f050009
-			public const int myButton = 2131034121;
+			// aapt resource value: 0x7f060009
+			public const int myButton = 2131099657;
 			
 			static Id()
 			{
@@ -186,6 +189,22 @@ namespace Andr.Unit
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int Mono_Android_Theme_Splash = 2131034112;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
