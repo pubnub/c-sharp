54d465e1260f3b17bf8bc7fd690a0a1708a627b5
Pandu Masabathula <pandumasabathula@Pandus-MacBook-Pro.local>
2016-03-29 19:27:46 +0530

    monoandroid:publish payload to success callback

--- mono-for-android/PubNubMessaging/PubnubXamarinAndroid.cs
+++ mono-for-android/PubNubMessaging/PubnubXamarinAndroid.cs
@@ -272,7 +272,7 @@ namespace PubNubMessaging.Core
                                 }
                                 if (!errorCallbackRaised)
                                 {
-                                    result = WrapResultBasedOnResponseType<T>(asynchRequestState.Type, jsonString, asynchRequestState.Channels, asynchRequestState.ChannelGroups, asynchRequestState.Reconnect, asynchRequestState.Timetoken, asynchRequestState.ErrorCallback);
+                                    result = WrapResultBasedOnResponseType<T>(asynchRequestState.Type, jsonString, asynchRequestState.Channels, asynchRequestState.ChannelGroups, asynchRequestState.Reconnect, asynchRequestState.Timetoken, asynchRequestState.Request, asynchRequestState.ErrorCallback);
                                 }
                             }
                         }
@@ -389,7 +389,7 @@ namespace PubNubMessaging.Core
                             } 
                             else if (jsonString != "[]") 
                             {
-                                result = WrapResultBasedOnResponseType<T>(asynchRequestState.Type, jsonString, asynchRequestState.Channels, asynchRequestState.ChannelGroups, asynchRequestState.Reconnect, asynchRequestState.Timetoken, asynchRequestState.ErrorCallback);
+                                result = WrapResultBasedOnResponseType<T>(asynchRequestState.Type, jsonString, asynchRequestState.Channels, asynchRequestState.ChannelGroups, asynchRequestState.Reconnect, asynchRequestState.Timetoken, asynchRequestState.Request, asynchRequestState.ErrorCallback);
                             } 
                             else 
                             {
@@ -1189,7 +1189,7 @@ namespace PubNubMessaging.Core
                 }
 
                 if (jsonString != null && !string.IsNullOrEmpty (jsonString) && !string.IsNullOrEmpty (channel.Trim ())) {
-                    result = WrapResultBasedOnResponseType<T> (asynchRequestState.Type, jsonString, asynchRequestState.Channels, asynchRequestState.ChannelGroups, asynchRequestState.Reconnect, asynchRequestState.Timetoken, asynchRequestState.ErrorCallback);
+        result = WrapResultBasedOnResponseType<T> (asynchRequestState.Type, jsonString, asynchRequestState.Channels, asynchRequestState.ChannelGroups, asynchRequestState.Reconnect, asynchRequestState.Timetoken, asynchRequestState.Request, asynchRequestState.ErrorCallback);
                 }
 
                 ProcessResponseCallbacks<T> (result, asynchRequestState);
