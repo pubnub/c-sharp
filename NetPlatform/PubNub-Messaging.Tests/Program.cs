fc1fe871b3f53e893bc565904b391f55f3ad21ce
Pandu <pandu@pubnub.com>
2016-06-22 16:44:07 +0530

    NetPlatform - Initial copy(Ref#19989)

---
+++ NetPlatform/PubNub-Messaging.Tests/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Reflection;
+using NUnit.Common;
+using NUnitLite;
+
+namespace PubNub_Messaging.Tests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new AutoRun(typeof(Program).GetTypeInfo().Assembly)
+                .Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
+
+            Console.ReadLine();
+        }
+    }
+}
