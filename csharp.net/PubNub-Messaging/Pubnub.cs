6bf988b471396f80be2b0309a143dd1347d1b648
Pandu <pandu@pubnub.com>
2016-06-25 07:57:53 +0530

    sdk version fix/check

--- csharp.net/PubNub-Messaging/Pubnub.cs
+++ csharp.net/PubNub-Messaging/Pubnub.cs
@@ -804,6 +804,14 @@ namespace PubNubMessaging.Core
                 pubnub.AddPayloadToPublishResponse = value;
             }
         }
+
+        public string Version
+        {
+            get
+            {
+                return pubnub.Version;
+            }
+        }
 		#endregion
 
 		#region "Constructors"
