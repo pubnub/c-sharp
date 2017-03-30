38907870412a319bd92e4d017b017591e96f37f3
Pandu <pandu@pubnub.com>
2017-03-30 14:06:18 +0530

    version updated to 4.0.1.4

--- src/Api/PubnubApi/Pubnub.cs
+++ src/Api/PubnubApi/Pubnub.cs
@@ -14,7 +14,7 @@ namespace PubnubApi
 
         private string instanceId = "";
 
-        private static string sdkVersion = "PubNubCSharp4.0.1.3";
+        private static string sdkVersion = "PubNubCSharp4.0.1.4";
 
         private object savedSubscribeOperation = null;
 
