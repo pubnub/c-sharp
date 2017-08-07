f19416c658ddba8587ac6c9edc01f0ae65d0987c
Pandu <pandu@pubnub.com>
2017-08-07 23:11:47 +0530

    uwp migration

--- src/Api/PubnubApiUWP/Properties/AssemblyInfo.cs
+++ src/Api/PubnubApiUWP/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using System.Reflection;
-//using System.Runtime.CompilerServices;
-//using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -14,14 +14,6 @@
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-//[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-//[assembly: Guid("015665a7-44d4-4119-971e-017b4707776a")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -32,5 +24,6 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.2.2")]
-[assembly: AssemblyFileVersion("4.0.2.2")]
+[assembly: AssemblyVersion("4.0.2.3")]
+[assembly: AssemblyFileVersion("4.0.2.3")]
+//[assembly: ComVisible(false)]
