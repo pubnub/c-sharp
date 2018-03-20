5a179eef0671d08104284f0b884441b472befea3
Pandu <pandu@pubnub.com>
2018-03-20 19:25:50 +0530

    network check time request non-200 status capture for logging

--- src/Api/PubnubApi/ClientNetworkStatus.cs
+++ src/Api/PubnubApi/ClientNetworkStatus.cs
@@ -160,11 +160,18 @@ namespace PubnubApi
                 myRequest.Method = "GET";
                 using (HttpWebResponse response = await Task.Factory.FromAsync<HttpWebResponse>(myRequest.BeginGetResponse, asyncPubnubResult => (HttpWebResponse)myRequest.EndGetResponse(asyncPubnubResult), null))
                 {
-                    if (response != null && response.StatusCode == HttpStatusCode.OK)
+                    if (response != null)
                     {
                         LoggingMethod.WriteToLog(pubnubLog, string.Format("DateTime {0} WebRequest CheckSocketConnect Resp {1}", DateTime.Now.ToString(CultureInfo.InvariantCulture), response.StatusCode.ToString()), pubnubConfig.LogVerbosity);
-                        networkStatus = true;
-                        t.TrySetResult(true);
+                        if (response.StatusCode == HttpStatusCode.OK)
+                        {
+                            networkStatus = true;
+                            t.TrySetResult(true);
+                        }
+                    }
+                    else
+                    {
+                        LoggingMethod.WriteToLog(pubnubLog, string.Format("DateTime {0} WebRequest CheckSocketConnect FAILED.", DateTime.Now.ToString(CultureInfo.InvariantCulture)), pubnubConfig.LogVerbosity);
                     }
                 }
 #endif
