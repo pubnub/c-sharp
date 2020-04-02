303328db01364e917bcdd94dc4554992c1190b48
Pandu Masabathula <pandu.masabathula@gmail.com>
2020-04-02 18:28:18 +0530

    minor update

--- src/Api/PubnubApi/EndPoint/Push/RemoveAllPushChannelsOperation.cs
+++ src/Api/PubnubApi/EndPoint/Push/RemoveAllPushChannelsOperation.cs
@@ -100,9 +100,9 @@ namespace PubnubApi.EndPoint
         public async Task<PNResult<PNPushRemoveAllChannelsResult>> ExecuteAsync()
         {
 #if NETFX_CORE || WINDOWS_UWP || UAP || NETSTANDARD10 || NETSTANDARD11 || NETSTANDARD12
-            return await RemoveAllChannelsForDevice(this.pubnubPushType, this.deviceTokenId, this.pushEnvironment, this.deviceTopic, this.queryParam);
+            return await RemoveAllChannelsForDevice(this.pubnubPushType, this.deviceTokenId, this.pushEnvironment, this.deviceTopic, this.queryParam).ConfigureAwait(false);
 #else
-            return await RemoveAllChannelsForDevice(this.pubnubPushType, this.deviceTokenId, this.pushEnvironment, this.deviceTopic, this.queryParam);
+            return await RemoveAllChannelsForDevice(this.pubnubPushType, this.deviceTokenId, this.pushEnvironment, this.deviceTopic, this.queryParam).ConfigureAwait(false);
 #endif
         }
 
