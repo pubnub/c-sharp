58deb9546a296160c25c3e4d9679049f6affd6d0
Mohit Tejani <mohit.tejani@pubnub.com>
2021-01-11 16:26:27 +0530

    * update Assembly Info and ,csproj files

--- src/Api/PubnubApi/Properties/AssemblyInfo.cs
+++ src/Api/PubnubApi/Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Pubnub C# SDK")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyVersion("4.12.0.0")]
-[assembly: AssemblyFileVersion("4.12.0.0")]
+[assembly: AssemblyVersion("4.12.1.0")]
+[assembly: AssemblyFileVersion("4.12.1.0")]
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
