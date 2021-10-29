ad8367ace08ce9744ac4df1d792784d4b443294b
Pandu Masabathula <budgetpreneur@users.noreply.github.com>
2021-10-29 18:09:45 +0530

    indentation

--- src/Api/PubnubApi/Model/ReconnectState.cs
+++ src/Api/PubnubApi/Model/ReconnectState.cs
@@ -4,21 +4,21 @@ namespace PubnubApi
 {
 	internal class ReconnectState<T>
 	{
-		public string[] Channels { get; internal set; }
-		public string[] ChannelGroups { get; internal set; }
-		public PNOperationType ResponseType { get; internal set; }
-		public PNCallback<T> PubnubCallback { get; internal set; }
-		public object Timetoken { get; internal set; }
-		public int Region { get; internal set; }
+        public string[] Channels { get; internal set; }
+        public string[] ChannelGroups { get; internal set; }
+        public PNOperationType ResponseType { get; internal set; }
+        public PNCallback<T> PubnubCallback { get; internal set; }
+        public object Timetoken { get; internal set; }
+        public int Region { get; internal set; }
 
-		public bool Reconnect { get; internal set; }
+        public bool Reconnect { get; internal set; }
 
-		public ReconnectState()
-		{
-			Channels = null;
-			ChannelGroups = null;
-			Timetoken = null;
-			Reconnect = false;
-		}
+        public ReconnectState()
+        {
+            Channels = null;
+            ChannelGroups = null;
+            Timetoken = null;
+            Reconnect = false;
+        }
 	}
 }
