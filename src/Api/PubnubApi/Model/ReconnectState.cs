745aaadc23648dc1474c656277fe0717983e5eab
Pandu Masabathula <budgetpreneur@users.noreply.github.com>
2021-10-29 18:00:27 +0530

    indentation fix

--- src/Api/PubnubApi/Model/ReconnectState.cs
+++ src/Api/PubnubApi/Model/ReconnectState.cs
@@ -9,7 +9,7 @@ namespace PubnubApi
         public PNOperationType ResponseType { get; internal set; }
         public PNCallback<T> PubnubCallback { get; internal set; }
         public object Timetoken { get; internal set; }
-		public int Region { get; internal set; }
+        public int Region { get; internal set; }
 
 		public bool Reconnect { get; internal set; }
 
