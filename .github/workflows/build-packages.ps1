1a89ba43120a8f4b0aa037a3ebb62e1d2bc61a27
Serhii Mamontov <parfeon@me.com>
2021-11-02 14:14:38 +0200

    update for tests

--- .github/workflows/build-packages.ps1
+++ .github/workflows/build-packages.ps1
@@ -1,21 +1,42 @@
 $githubWorkspace = $args[0]
+$secret = $args[1]
 
-
+# Create required directory structure.
 $RELEASES_PATH = "$githubWorkspace\.github\.release"
+$ARTIFACTS_PATH = "$RELEASES_PATH\artifacts"
 mkdir "$RELEASES_PATH"
+mkdir "$RELEASES_PATH\Api"
+mkdir "$RELEASES_PATH\PCL"
+mkdir "$RELEASES_PATH\UWP"
+mkdir "$ARTIFACTS_PATH"
+
+Write-Output "~~~~> FIRST ARGUMENT: '$githubWorkspace'"
+Write-Output "~~~~> SECOND ARGUMENT: '$secret'"
+
 
 # Build Api package.
 cd "$githubWorkspace\src\Api\PubnubApi"
-#dotnet pack -o "$RELEASES_PATH\Api" -c Release
-dotnet pack -o "$RELEASES_PATH" -c Release
+dotnet pack -o "$RELEASES_PATH\Api" -c Release
 
 # Build PCL package.
 cd "$githubWorkspace\src\Api\PubnubApiPCL"
-#dotnet pack -o "$RELEASES_PATH\PCL" -c Release
-dotnet pack -o "$RELEASES_PATH" -c Release
+dotnet pack -o "$RELEASES_PATH\PCL" -c Release
 
 # Build UWP package.
 cd "$githubWorkspace\src\Api\PubnubApiUWP"
 dotnet restore
-#msbuild PubnubApiUWP.csproj /t:Pack /p:Configuration=Release /p:PackageOutputPath="$RELEASES_PATH\UWP" /v:n
-msbuild PubnubApiUWP.csproj /t:Pack /p:Configuration=Release /p:PackageOutputPath="$RELEASES_PATH" /v:n
+msbuild PubnubApiUWP.csproj /t:Pack /p:Configuration=Release /p:PackageOutputPath="$RELEASES_PATH\UWP" /v:n
+
+
+# Copy built packages to artifacts folder.
+$PACKAGE_NAME = Get-ChildItem -Name -Path "$RELEASES_PATH\Api\*" -Include *.nupkg
+$PACKAGE_PATH = "$RELEASES_PATH\Api\$PACKAGE_NAME"
+cp "$PACKAGE_PATH" "$ARTIFACTS_PATH\$PACKAGE_NAME"
+
+$PCL_PACKAGE_NAME = Get-ChildItem -Name -Path "$RELEASES_PATH\PCL\*" -Include *.nupkg
+$PCL_PACKAGE_PATH = "$RELEASES_PATH\PCL\$PCL_PACKAGE_NAME"
+cp "$PCL_PACKAGE_PATH" "$ARTIFACTS_PATH\$PCL_PACKAGE_NAME"
+
+$UWP_PACKAGE_NAME = Get-ChildItem -Name -Path "$RELEASES_PATH\UWP\*" -Include *.nupkg
+$UWP_PACKAGE_PATH = "$RELEASES_PATH\UWP\$UWP_PACKAGE_NAME"
+cp "$UWP_PACKAGE_PATH" "$ARTIFACTS_PATH\$UWP_PACKAGE_NAME"
