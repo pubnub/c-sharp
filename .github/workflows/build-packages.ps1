1907aaf93e29920606392ef655ee3fd0f253b4e7
Serhii Mamontov <parfeon@me.com>
2021-11-02 12:02:35 +0200

    update for tests

--- .github/workflows/build-packages.ps1
+++ .github/workflows/build-packages.ps1
@@ -6,13 +6,16 @@ mkdir "$RELEASES_PATH"
 
 # Build Api package.
 cd "$githubWorkspace\src\Api\PubnubApi"
-dotnet pack -o "$RELEASES_PATH\Api" -c Release
+#dotnet pack -o "$RELEASES_PATH\Api" -c Release
+dotnet pack -o "$RELEASES_PATH" -c Release
 
 # Build PCL package.
 cd "$githubWorkspace\src\Api\PubnubApiPCL"
-dotnet pack -o "$RELEASES_PATH\PCL" -c Release
+#dotnet pack -o "$RELEASES_PATH\PCL" -c Release
+dotnet pack -o "$RELEASES_PATH" -c Release
 
 # Build UWP package.
 cd "$githubWorkspace\src\Api\PubnubApiUWP"
 dotnet restore
-msbuild PubnubApiUWP.csproj /t:Pack /p:Configuration=Release /p:PackageOutputPath="$RELEASES_PATH\UWP" /v:n
+#msbuild PubnubApiUWP.csproj /t:Pack /p:Configuration=Release /p:PackageOutputPath="$RELEASES_PATH\UWP" /v:n
+msbuild PubnubApiUWP.csproj /t:Pack /p:Configuration=Release /p:PackageOutputPath="$RELEASES_PATH" /v:n
