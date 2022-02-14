6f80a0e74d34c19788f647ded4fa747090ee3bfe
Pandu Masabathula <budgetpreneur@users.noreply.github.com>
2022-02-14 16:18:49 +0530

    States and Events

--- src/Api/PubnubApi/EndPoint/PubSub/StatelessSubscribe.cs
+++ src/Api/PubnubApi/EndPoint/PubSub/StatelessSubscribe.cs
@@ -87,4 +87,167 @@ namespace PubnubApi.EndPoint
         }
     }
 
+    public class SubscribeStateMachine
+    {
+        public enum SubscribeState
+        {
+            HandshakeFailed,
+            Unsubscribed,
+            ReconnectionFailed,
+            Handshaking,
+            Preparing,
+            Stopped,
+            Reconnecting,
+            Receiving,
+            Delivering
+        }
+
+        public enum SubscribeEvent
+        {
+            Reconnect,
+            SubscriptionChange,
+            Fail,
+            Giveup,
+            Success,
+            Subscribe,
+            Restore,
+            Disconnect,
+            Done
+        }
+
+        public SubscribeState ChangeSubscribeState(SubscribeState current, SubscribeEvent subscribeEvt)
+        {
+            SubscribeState ret;
+            switch (current)
+            {
+                case SubscribeState.Handshaking:
+                    switch (subscribeEvt)
+                    {
+                        case SubscribeEvent.SubscriptionChange:
+                        case SubscribeEvent.Fail:
+                            ret = SubscribeState.Handshaking;
+                            break;
+                        case SubscribeEvent.Success:
+                            ret = SubscribeState.Receiving;
+                            break;
+                        case SubscribeEvent.Giveup:
+                            ret = SubscribeState.HandshakeFailed;
+                            break;
+                        default:
+                            throw new Exception($"State: {current}; Event:{subscribeEvt} has no transition/effect");
+                    }
+                    break;
+                case SubscribeState.HandshakeFailed:
+                    switch (subscribeEvt)
+                    {
+                        case SubscribeEvent.SubscriptionChange:
+                        case SubscribeEvent.Reconnect:
+                            ret = SubscribeState.Handshaking;
+                            break;
+                        default:
+                            throw new Exception($"State: {current}; Event:{subscribeEvt} has no transition/effect");
+                    }
+                    break;
+                case SubscribeState.Reconnecting:
+                    switch (subscribeEvt)
+                    {
+                        case SubscribeEvent.SubscriptionChange:
+                        case SubscribeEvent.Fail:
+                            ret = SubscribeState.Reconnecting;
+                            break;
+                        case SubscribeEvent.Success:
+                            ret = SubscribeState.Delivering;
+                            break;
+                        case SubscribeEvent.Giveup:
+                            ret = SubscribeState.ReconnectionFailed;
+                            break;
+                        default:
+                            throw new Exception($"State: {current}; Event:{subscribeEvt} has no transition/effect");
+                    }
+                    break;
+                case SubscribeState.ReconnectionFailed:
+                    switch (subscribeEvt)
+                    {
+                        case SubscribeEvent.SubscriptionChange:
+                        case SubscribeEvent.Reconnect:
+                            ret = SubscribeState.Reconnecting;
+                            break;
+                        default:
+                            throw new Exception($"State: {current}; Event:{subscribeEvt} has no transition/effect");
+                    }
+                    break;
+                case SubscribeState.Unsubscribed:
+                    switch (subscribeEvt)
+                    {
+                        case SubscribeEvent.Subscribe:
+                            ret = SubscribeState.Handshaking;
+                            break;
+                        case SubscribeEvent.Restore:
+                            ret = SubscribeState.Reconnecting;
+                            break;
+                        default:
+                            throw new Exception($"State: {current}; Event:{subscribeEvt} has no transition/effect");
+                    }
+                    break;
+                case SubscribeState.Preparing:
+                    switch (subscribeEvt)
+                    {
+                        case SubscribeEvent.Reconnect:
+                            ret = SubscribeState.Handshaking;
+                            break;
+                        case SubscribeEvent.SubscriptionChange:
+                            ret = SubscribeState.Preparing;
+                            break;
+                        default:
+                            throw new Exception($"State: {current}; Event:{subscribeEvt} has no transition/effect");
+                    }
+                    break;
+                case SubscribeState.Stopped:
+                    switch (subscribeEvt)
+                    {
+                        case SubscribeEvent.Reconnect:
+                            ret = SubscribeState.Reconnecting;
+                            break;
+                        case SubscribeEvent.SubscriptionChange:
+                            ret = SubscribeState.Stopped;
+                            break;
+                        default:
+                            throw new Exception($"State: {current}; Event:{subscribeEvt} has no transition/effect");
+                    }
+                    break;
+                case SubscribeState.Receiving:
+                    switch (subscribeEvt)
+                    {
+                        case SubscribeEvent.Success:
+                            ret = SubscribeState.Delivering;
+                            break;
+                        case SubscribeEvent.Fail:
+                            ret = SubscribeState.Reconnecting;
+                            break;
+                        case SubscribeEvent.SubscriptionChange:
+                            ret = SubscribeState.Receiving;
+                            break;
+                        default:
+                            throw new Exception($"State: {current}; Event:{subscribeEvt} has no transition/effect");
+                    }
+                    break;
+                case SubscribeState.Delivering:
+                    switch (subscribeEvt)
+                    {
+                        case SubscribeEvent.Done:
+                            ret = SubscribeState.Receiving;
+                            break;
+                        default:
+                            throw new Exception($"State: {current}; Event:{subscribeEvt} has no transition/effect");
+                    }
+                    break;
+                default:
+                    throw new Exception($"State: {current}; Event:{subscribeEvt} has no transition/effect");
+            }
+
+            return ret;
+
+        }
+
+    }
 }
