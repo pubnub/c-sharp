0008dbd52274539b526c878bfbb1d12996d18e62
Pandu Masabathula <budgetpreneur@users.noreply.github.com>
2022-03-23 11:24:08 +0530

    restore nuget push

--- .github/workflows/release/publish-packages.ps1
+++ .github/workflows/release/publish-packages.ps1
@@ -6,14 +6,14 @@ $RELEASES_PATH = "$githubWorkspace\.github\.release"
 # Publish package
 $PACKAGE_NAME = Get-ChildItem -Name -Path "$RELEASES_PATH\Api\*" -Include *.nupkg
 $PACKAGE_PATH = "$RELEASES_PATH\Api\$PACKAGE_NAME"
-# nuget.exe push $PACKAGE_PATH -Source "https://www.nuget.org" -ApiKey $nugetApiKey
+nuget.exe push $PACKAGE_PATH -Source "https://www.nuget.org" -ApiKey $nugetApiKey
 
 # Publish PCL package.
 $PCL_PACKAGE_NAME = Get-ChildItem -Name -Path "$RELEASES_PATH\PCL\*" -Include *.nupkg
 $PCL_PACKAGE_PATH = "$RELEASES_PATH\PCL\$PCL_PACKAGE_NAME"
-# nuget.exe push $PCL_PACKAGE_PATH -Source "https://www.nuget.org" -ApiKey $nugetApiKey
+nuget.exe push $PCL_PACKAGE_PATH -Source "https://www.nuget.org" -ApiKey $nugetApiKey
 
 # Publish UWP package.
 $UWP_PACKAGE_NAME = Get-ChildItem -Name -Path "$RELEASES_PATH\UWP\*" -Include *.nupkg
 $UWP_PACKAGE_PATH = "$RELEASES_PATH\UWP\$UWP_PACKAGE_NAME"
-# nuget.exe push $UWP_PACKAGE_PATH -Source "https://www.nuget.org" -ApiKey $nugetApiKey
+nuget.exe push $UWP_PACKAGE_PATH -Source "https://www.nuget.org" -ApiKey $nugetApiKey
