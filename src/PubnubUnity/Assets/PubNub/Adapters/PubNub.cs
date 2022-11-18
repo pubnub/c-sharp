75e8631dd8b25d61747cdf1abcf45d69ff6d9e77
Michał Dobrzański <michal.dobrzanski@pubnub.com>
2022-11-18 14:31:21 +0100

    fix: obsoleting adapters

--- src/PubnubUnity/Assets/PubNub/Adapters/PubNub.cs
+++ src/PubnubUnity/Assets/PubNub/Adapters/PubNub.cs
@@ -7,11 +7,17 @@ using PubnubApi.EndPoint;
 namespace PubnubApi.Unity {
 	public static class PubnubExtensions {
 		public static SubscribeOperation<T> Subscribe<T>(this Pubnub pn) => pn.Subscribe<T>();
+		
+		[Obsolete("Use the generic version instead")]
 		public static SubscribeOperation<string> Subscribe(this Pubnub pn) => pn.Subscribe<string>();
+
+		[Obsolete("Use the generic version instead")]
+		public static bool Reconnect(this Pubnub pn) => pn.Reconnect<string>();
 
 
 		public delegate void Listener(object sender, EventArgs args);
 
+		[Obsolete("Use the overload accepting SubscribeCallback instead")]
 		public static bool AddListener(this Pubnub pn, Listener listener) => pn.AddListener(new SubscribeCallbackExt(
 			(s, r) => listener(s, new SubscribeEventEventArgs<object>() { MessageResult = r }),
 			(s, r) => listener(s, new SubscribeEventEventArgs<object>() { PresenceEventResult = r }),
