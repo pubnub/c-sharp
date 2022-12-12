ac8c3e9597bc8935618cd50c9c761c8b3d1b1546
Michał Dobrzański <michal.dobrzanski@pubnub.com>
2022-12-12 20:35:03 +0100

    added constructor for similarity with SubscribeCallbackExt class

--- src/PubnubUnity/Assets/PubNub/Adapters/SubscribeCallbackListener.cs
+++ src/PubnubUnity/Assets/PubNub/Adapters/SubscribeCallbackListener.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 using PubnubApi.Unity.Internal;
 
 namespace PubnubApi.Unity {
+	/// <summary>
+	/// Wrapper for the SubscribeCallback class. Allows dispatching events to main game loop, and direct subscription to every PubNub event.
+	/// </summary>
 	public class SubscribeCallbackListener {
 		public event Action<Pubnub, PNMessageResult<object>> onMessage;
 		public event Action<Pubnub, PNPresenceEventResult> onPresence;
@@ -13,6 +16,22 @@ namespace PubnubApi.Unity {
 		public event Action<Pubnub, PNStatus> onStatus;
 
 		protected SubscribeCallbackExt listener;
+
+		public SubscribeCallbackListener(Action<Pubnub, PNMessageResult<object>> messageCallback,
+			Action<Pubnub, PNPresenceEventResult> presenceCallback,
+			Action<Pubnub, PNSignalResult<object>> signalCallback,
+			Action<Pubnub, PNObjectEventResult> objectEventCallback,
+			Action<Pubnub, PNMessageActionEventResult> messageActionCallback,
+			Action<Pubnub, PNFileEventResult> fileCallback,
+			Action<Pubnub, PNStatus> statusCallback) : this() {
+			this.onMessage += messageCallback;
+			this.onPresence += presenceCallback;
+			this.onSignal += signalCallback;
+			this.onObject += objectEventCallback;
+			this.onMessageAction += messageActionCallback;
+			this.onFile += fileCallback;
+			this.onStatus += statusCallback;
+		}
 
 		public SubscribeCallbackListener() {
 			this.listener = new SubscribeCallbackExt(
