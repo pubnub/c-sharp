f854f905a4ec6f87f62b500774e7aa6242ca2775
Pandu Masabathula <budgetpreneur@users.noreply.github.com>
2022-12-15 13:44:41 +0530

    WIP - Publish Steps

--- src/UnitTests/AcceptanceTests/Steps/PublishAMessageSteps.cs
+++ src/UnitTests/AcceptanceTests/Steps/PublishAMessageSteps.cs
@@ -1,56 +1,233 @@
 using System;
 using TechTalk.SpecFlow;
+using PubnubApi;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Net;
+using System.Globalization;
+using System.IO;
+using System.Text.Json;
+using static AcceptanceTests.Steps.FeatureObjectsV2MetadataSteps;
 
 namespace AcceptanceTests.Steps
 {
     [Binding]
     public class FeaturePublishAMessageSteps
     {
-        public FeaturePublishAMessageSteps() 
+        public static bool enableIntenalPubnubLogging = false;
+        public static string currentFeature = string.Empty;
+        public static string currentContract = string.Empty;
+        public static bool betaVersion = false;
+        private string acceptance_test_origin = "localhost:8090";
+        private bool bypassMockServer = false;
+        private readonly ScenarioContext _scenarioContext;
+        private Pubnub pn;
+        private PNConfiguration config = null;
+
+        private PNPublishResult getPublishResult = null;
+        private PNStatus pnStatus = null;
+        private PubnubError pnError = null;
+
+
+        public class InternalPubnubLog : IPubnubLog
+        {
+            void IPubnubLog.WriteToLog(string logText)
+            {
+                System.Diagnostics.Debug.WriteLine(logText);
+                string dirPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                string logFilePath = System.IO.Path.Combine(dirPath, "pubnubmessaging.log");
+                System.Diagnostics.Trace.Listeners.Add(new System.Diagnostics.TextWriterTraceListener(logFilePath));
+
+            }
+        }
+        public FeaturePublishAMessageSteps(ScenarioContext scenarioContext) 
         { 
-        
+            _scenarioContext = scenarioContext;
+        }
+
+        [BeforeFeature]
+        public static void BeforeFeature(FeatureContext featureContext)
+        {
+            betaVersion = false;
+            if (featureContext.FeatureInfo != null && featureContext.FeatureInfo.Tags.Length > 0)
+            {
+                List<string> tagList = featureContext.FeatureInfo.Tags.AsEnumerable<string>().ToList();
+                foreach (string tag in tagList)
+                {
+                    if (tag.IndexOf("featureSet=") == 0)
+                    {
+                        currentFeature = tag.Replace("featureSet=", "");
+                    }
+
+                    if (tag.IndexOf("beta") == 0)
+                    {
+                        betaVersion = true;
+                    }
+                }
+            }
+
+            System.Diagnostics.Debug.WriteLine("Starting " + featureContext.FeatureInfo.Title);
+        }
+
+        [AfterFeature]
+        public static void AfterFeature(FeatureContext featureContext)
+        {
+            System.Diagnostics.Debug.WriteLine("Finished " + featureContext.FeatureInfo.Title);
+        }
+
+        [BeforeScenario()]
+        public void BeforeScenario()
+        {
+            currentContract = "";
+            if (_scenarioContext.ScenarioInfo != null && _scenarioContext.ScenarioInfo.Tags.Length > 0)
+            {
+                List<string> tagList = _scenarioContext.ScenarioInfo.Tags.AsEnumerable<string>().ToList();
+                foreach (string tag in tagList)
+                {
+                    if (tag.IndexOf("contract=") == 0)
+                    {
+                        currentContract = tag.Replace("contract=", "");
+                        break;
+                    }
+                }
+                if (!string.IsNullOrEmpty(currentContract) && !bypassMockServer)
+                {
+                    string mockInitContract = string.Format("http://{0}/init?__contract__script__={1}", acceptance_test_origin, currentContract);
+                    System.Diagnostics.Debug.WriteLine(mockInitContract);
+                    WebClient webClient = new WebClient();
+                    string mockInitResponse = webClient.DownloadString(mockInitContract);
+                    System.Diagnostics.Debug.WriteLine(mockInitResponse);
+                }
+            }
+
+        }
+
+        [AfterScenario()]
+        public void AfterScenario()
+        {
+            if (!bypassMockServer)
+            {
+                string mockExpectContract = string.Format("http://{0}/expect", acceptance_test_origin);
+                System.Diagnostics.Debug.WriteLine(mockExpectContract);
+                WebClient webClient = new WebClient();
+                string mockExpectResponse = webClient.DownloadString(mockExpectContract);
+                System.Diagnostics.Debug.WriteLine(mockExpectResponse);
+            }
         }
 
         [Given(@"the demo keyset")]
         public void GivenTheDemoKeyset()
         {
-            throw new PendingStepException();
+            config = new PNConfiguration(new UserId("pn-csharp-acceptance-test-uuid"));
+            config.Origin = acceptance_test_origin;
+            config.Secure = false;
+            config.PublishKey = System.Environment.GetEnvironmentVariable("PN_PUB_KEY");
+            config.SubscribeKey = System.Environment.GetEnvironmentVariable("PN_SUB_KEY");
+            config.SecretKey = System.Environment.GetEnvironmentVariable("PN_SEC_KEY");
+            if (enableIntenalPubnubLogging)
+            {
+                config.LogVerbosity = PNLogVerbosity.BODY;
+                config.PubnubLog = new InternalPubnubLog();
+            }
+            else
+            {
+                config.LogVerbosity = PNLogVerbosity.NONE;
+            }
+
+            pn = new Pubnub(config);
         }
 
         [When(@"I publish a message")]
-        public void WhenIPublishAMessage()
+        public async Task WhenIPublishAMessage()
         {
-            throw new PendingStepException();
+            PNResult<PNPublishResult> getPublishResponse = await pn.Publish()
+                .Channel("my_channel")
+                .Message("test message")
+                .ExecuteAsync();
+
+            getPublishResult = getPublishResponse.Result;
+            pnStatus = getPublishResponse.Status;
+            if (pnStatus.Error)
+            {
+                pnError = pn.JsonPluggableLibrary.DeserializeToObject<PubnubError>(pnStatus.ErrorData.Information);
+            }
         }
 
         [Then(@"I receive successful response")]
         public void ThenIReceiveSuccessfulResponse()
         {
-            throw new PendingStepException();
+            Assert.IsTrue(!pnStatus.Error);
         }
 
         [When(@"I publish a message with JSON metadata")]
-        public void WhenIPublishAMessageWithJSONMetadata()
+        public async Task WhenIPublishAMessageWithJSONMetadata()
         {
-            throw new PendingStepException();
+            Dictionary<string, object> dictMeta = new Dictionary<string, object>();
+            dictMeta.Add("color","green");
+            dictMeta.Add("empid", 123);
+
+            PNResult<PNPublishResult> getPublishResponse = await pn.Publish()
+                .Channel("my_channel")
+                .Message("test message")
+                .Meta(dictMeta)
+                .ExecuteAsync();
+
+            getPublishResult = getPublishResponse.Result;
+            pnStatus = getPublishResponse.Status;
+            if (pnStatus.Error)
+            {
+                pnError = pn.JsonPluggableLibrary.DeserializeToObject<PubnubError>(pnStatus.ErrorData.Information);
+            }
         }
 
         [When(@"I publish a message with string metadata")]
-        public void WhenIPublishAMessageWithStringMetadata()
+        public async Task WhenIPublishAMessageWithStringMetadata()
         {
-            throw new PendingStepException();
+            Dictionary<string, object> dictMeta = new Dictionary<string, object>();
+            dictMeta.Add("color","green");
+
+            PNResult<PNPublishResult> getPublishResponse = await pn.Publish()
+                .Channel("my_channel")
+                .Message("test message")
+                .Meta(dictMeta)
+                .ExecuteAsync();
+
+            getPublishResult = getPublishResponse.Result;
+            pnStatus = getPublishResponse.Status;
+            if (pnStatus.Error)
+            {
+                pnError = pn.JsonPluggableLibrary.DeserializeToObject<PubnubError>(pnStatus.ErrorData.Information);
+            }
         }
 
         [Given(@"the invalid keyset")]
         public void GivenTheInvalidKeyset()
         {
-            throw new PendingStepException();
+            config = new PNConfiguration(new UserId("pn-csharp-acceptance-test-uuid"));
+            config.Origin = acceptance_test_origin;
+            config.Secure = false;
+            config.PublishKey = "wrong_pub_key";
+            config.SubscribeKey = "wrong_sub_key";
+            config.SecretKey = "wrong_secret_key";
+            if (enableIntenalPubnubLogging)
+            {
+                config.LogVerbosity = PNLogVerbosity.BODY;
+                config.PubnubLog = new InternalPubnubLog();
+            }
+            else
+            {
+                config.LogVerbosity = PNLogVerbosity.NONE;
+            }
+
+            pn = new Pubnub(config);
         }
 
         [Then(@"I receive error response")]
         public void ThenIReceiveErrorResponse()
         {
-            throw new PendingStepException();
+            Assert.IsTrue(pnStatus.Error);
         }
     }
 }
