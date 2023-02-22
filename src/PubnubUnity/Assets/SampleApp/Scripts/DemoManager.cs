b359b77fae68b8b5856d160d7eedb76356ead6c9
Michał Dobrzański <michal.dobrzanski@pubnub.com>
2023-02-22 10:50:25 +0100

    WIP

--- src/PubnubUnity/Assets/SampleApp/Scripts/DemoManager.cs
+++ src/PubnubUnity/Assets/SampleApp/Scripts/DemoManager.cs
@@ -8,7 +8,8 @@ using UnityEngine;
 public class DemoManager : PNManagerBehaviour {
 	public string defaultUserId;
 	public string defaultChannel;
-
+	
+	public static DemoManager instance { get; }
 
 	protected override void Awake() {
 		base.Awake();
@@ -16,10 +17,23 @@ public class DemoManager : PNManagerBehaviour {
 		pubnub.Subscribe<string>().Channels(new[] { defaultChannel }).Execute();
 		
 		listener.onMessage += OnPnMessage;
+		listener.onSignal += OnPnSignal;
+	}
+
+	private void OnPnSignal(Pubnub arg1, PNSignalResult<object> arg2) {
+		Debug.Log(arg2.Message);
 	}
 
 	private async void Start() {
+		// async version
 		var result = await pubnub.Publish().Channel(defaultChannel).Message("hello there").ExecuteAsync();
+
+		// callback version
+		pubnub.Publish().Channel(defaultChannel).Message("hello there 2").Execute((a, b) => Debug.Log("Published"));
+		
+		// let's signal
+		pubnub.Signal().Channel(defaultChannel).Message("SampleSignal").Execute((a, b) => Debug.Log("Signalled"));
+		
 		if (!result.Status.Error) {
 			Debug.Log("Message sent successfully");
 		}
