0dd18b410d06d412b4082a7cba70792d34fd8ea0
Michał Dobrzański <michal.dobrzanski@pubnub.com>
2023-02-28 10:45:42 +0100

    WIP: Serialize quaternions

--- src/PubnubUnity/Assets/PubNub/Util/VectorSerializable.cs
+++ src/PubnubUnity/Assets/PubNub/Util/VectorSerializable.cs
@@ -8,11 +8,13 @@ namespace PubnubApi.Unity {
 		public float x;
 		public float y;
 		public float z;
+		public float w;
 
-		public VectorSerializable(float x, float y, float z) {
+		public VectorSerializable(float x, float y, float z = 0f, float w = 0f) {
 			this.x = x;
 			this.y = y;
 			this.z = z;
+			this.w = w;
 		}
 
 		public VectorSerializable(Vector3 sourceVector) {
@@ -24,7 +26,13 @@ namespace PubnubApi.Unity {
 		public VectorSerializable(Vector2 sourceVector) {
 			x = sourceVector.x;
 			y = sourceVector.y;
-			z = 0;
+		}
+
+		public VectorSerializable(Quaternion sourceQuaternion) {
+			x = sourceQuaternion.x;
+			y = sourceQuaternion.y;
+			z = sourceQuaternion.z;
+			w = sourceQuaternion.w;
 		}
 
 		public static implicit operator Vector3(VectorSerializable v) {
@@ -34,6 +42,10 @@ namespace PubnubApi.Unity {
 		public static implicit operator Vector2(VectorSerializable v) {
 			return new Vector2(v.x, v.y);
 		}
+		
+		public static implicit operator Quaternion(VectorSerializable v) {
+			return new Quaternion(v.x, v.y, v.z, v.w);
+		}
 
 		public static implicit operator VectorSerializable(Vector3 v) {
 			return new VectorSerializable(v);
@@ -42,12 +54,19 @@ namespace PubnubApi.Unity {
 		public static implicit operator VectorSerializable(Vector2 v) {
 			return new VectorSerializable(v);
 		}
+		
+		public static implicit operator VectorSerializable(Quaternion v) {
+			return new VectorSerializable(v);
+		}
+		
 
 		// TODO investigate if it's worth adding other json libraries
 		[JsonIgnore]
 		public Vector2 Vector2 => this;
 		[JsonIgnore]
 		public Vector3 Vector3 => this;
+		[JsonIgnore]
+		public Quaternion Quaternion => this;
 	}
    
 	public static class VectorExtensions {
@@ -56,6 +75,10 @@ namespace PubnubApi.Unity {
 		}
 
 		public static VectorSerializable Serializable(this Vector2 v) {
+			return v;
+		}
+		
+		public static VectorSerializable Serializable(this Quaternion v) {
 			return v;
 		}
 	}
