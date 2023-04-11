5132d4ef9c1b18daec849a379862a2995763aa8e
Michał Dobrzański <michal.dobrzanski@pubnub.com>
2023-04-11 11:46:35 +0200

    WIP

--- src/PubnubUnity/Assets/PubNub/Adapters/SendReceiveFileOperations.cs
+++ src/PubnubUnity/Assets/PubNub/Adapters/SendReceiveFileOperations.cs
@@ -1,9 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using PubnubApi.EndPoint;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 
-namespace PubnubApi.Unity {
+// File operation convenience methods
+namespace PubnubApi.Unity.FileOperations {
 	public static class SendReceiveFileOperations {
+		/// <summary>
+		/// Converts a texture into bytes and uploads it to PubNub.
+		///
+		/// This method overrides <c>Message</c> with input texture's size and format.
+		/// </summary>
+		/// <example>
+		/// <c>var res = await pubnub.SendFile().Texture(texture).Channel("test").FileName("texture.tex").ExecuteAsync()</c>;
+		/// </example>
+		/// <param name="texture">A Texture compatible with <c>GetRawTextureData</c></param>
+		/// <returns></returns>
+		public static SendFileOperation Texture(this SendFileOperation o, Texture texture) {
+			if ((texture as Texture2D) is null) {
+				Debug.LogError("Texture cannot be converted");
+				return o;
+			}
+			
+			return o.File(((Texture2D)texture).GetRawTextureData()).Message(new VectorSerializable(texture.width, texture.height, (int)texture.graphicsFormat));
+		}
+
+		public static async Task<Texture> DownloadTexture(this Pubnub pubnub, PNFileEventResult fileEventResult,  string channel, TextureCreationFlags textureCreationFlags = TextureCreationFlags.None) {
+			var size = new VectorSerializable(fileEventResult.Message as Dictionary<string, object>);
+			var f = await pubnub.DownloadFile().FileId(fileEventResult.File.Id).FileName(fileEventResult.File.Name).Channel(channel).ExecuteAsync();
 		
+			var tex = new Texture2D((int)size.x, (int)size.y, (GraphicsFormat)size.z, 0);
+			tex.LoadRawTextureData(f.Result.FileBytes);
+			tex.Apply();
+			return tex;
+		}
 	}
 }
--- src/PubnubUnity/Assets/SampleApp/Scripts/PnImageSendDemo.cs
+++ src/PubnubUnity/Assets/SampleApp/Scripts/PnImageSendDemo.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using PubnubApi;
 using PubnubApi.Unity;
+using PubnubApi.Unity.FileOperations;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
 using UnityEngine.UI;
@@ -24,13 +25,14 @@ public class PnImageSendDemo : MonoBehaviour {
 	}
 
 	private async void PnOnFile(Pubnub pn, PNFileEventResult file) {
-		var size = new VectorSerializable(file.Message as Dictionary<string, object>);
-		var f = await pnManager.pubnub.DownloadFile().FileId(file.File.Id).FileName(file.File.Name).Channel("test").ExecuteAsync();
-		
-		var tex = new Texture2D((int)size.x, (int)size.y, GraphicsFormat.RGBA_DXT1_UNorm, 0);
-		tex.LoadRawTextureData(f.Result.FileBytes);
-		tex.Apply();
-		targetImageComponent.texture = tex;
+		// var size = new VectorSerializable(file.Message as Dictionary<string, object>);
+		// var f = await pnManager.pubnub.DownloadFile().FileId(file.File.Id).FileName(file.File.Name).Channel("test").ExecuteAsync();
+		//
+		// var tex = new Texture2D((int)size.x, (int)size.y, GraphicsFormat.RGBA_DXT1_UNorm, 0);
+		// tex.LoadRawTextureData(f.Result.FileBytes);
+		// tex.Apply();
+		// targetImageComponent.texture = tex;
+		targetImageComponent.texture = await pn.DownloadTexture(file, "test", TextureCreationFlags.None);
 	}
 
 	async void Update() {
@@ -39,9 +41,11 @@ public class PnImageSendDemo : MonoBehaviour {
 			onTextureChange?.Invoke(sourceImageComponent.texture);
 			previousTexture = sourceImageComponent.texture;
 
-			var bytes = (previousTexture as Texture2D).GetRawTextureData();
-
-			var res = await pnManager.pubnub.SendFile().File(bytes).Channel("test").Message(new VectorSerializable(previousTexture.width, previousTexture.height)).FileName("dekzdura.tex").ExecuteAsync();
+			// var bytes = (previousTexture as Texture2D).GetRawTextureData();
+			//
+			// var res = await pnManager.pubnub.SendFile().File(bytes).Channel("test").Message(new VectorSerializable(previousTexture.width, previousTexture.height, (int)previousTexture.graphicsFormat)).FileName("dekzdura.tex").ExecuteAsync();
+			var res = await pnManager.pubnub.SendFile().Texture(previousTexture).Channel("test")
+				.FileName("degzdura.tex").ExecuteAsync();
 			Debug.Log(res.Status.Error);
 		}
 	}
