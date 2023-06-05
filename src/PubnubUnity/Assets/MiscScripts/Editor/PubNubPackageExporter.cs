752ee65a43e7a87b4502f9d36a13ceb2df5d124d
Michał Dobrzański <michal.dobrzanski@pubnub.com>
2023-06-05 13:18:16 +0200

    WIP exporter

--- src/PubnubUnity/Assets/MiscScripts/Editor/PubNubPackageExporter.cs
+++ src/PubnubUnity/Assets/MiscScripts/Editor/PubNubPackageExporter.cs
@@ -7,26 +7,25 @@ namespace PubnubUtils {
 	public class PubNubPackageExporter : MonoBehaviour {
 		[MenuItem("Assets/Export PubNub Package")]
 		public static async void ExportPNPackage() {
-			var assets = new[] { "Assets/PubNub", "Assets/ScriptTemplates" };
+			var assets = new[] { "Assets/PubNub/UnitySDK" };
 
 			Debug.Log("Assets to be exported:\n" + string.Join(", ", assets));
 
 			var exportMethod = Assembly.Load("asset-store-tools-editor")
 				.GetType("AssetStoreTools.Uploader.PackageExporter")
 				.GetMethod("ExportPackage", BindingFlags.Static | BindingFlags.Public | BindingFlags.FlattenHierarchy);
-			
+
 			var task = exportMethod.Invoke(
 				null,
-				new object[]
-					{ 
-						assets, 
-						"PubNub.unitypackage", 
-						true, 
-						false, 
-						false, 
-						new[] { "com.unity.nuget.newtonsoft-json" } 
-					}
-				) as Task;
+				new object[] {
+					assets,
+					"PubNub.unitypackage",
+					true,
+					false,
+					false,
+					new[] { "com.unity.nuget.newtonsoft-json" }
+				}
+			) as Task;
 
 			await task;
 			var o = task
@@ -36,7 +35,7 @@ namespace PubnubUtils {
 			var r = o.GetType()
 				.GetField("Success")
 				.GetValue(o) as bool?;
-			
+
 			Debug.Assert(r.Value, "Export broke.");
 		}
 	}
