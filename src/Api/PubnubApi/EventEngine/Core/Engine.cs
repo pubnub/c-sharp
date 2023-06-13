5ff6ff38da9ee703cecb48eecc711e7a9e62131e
Michał Dobrzański <michal.dobrzanski@pubnub.com>
2023-06-13 09:45:07 +0200

    WIP - eventQueue

--- src/Api/PubnubApi/EventEngine/Core/Engine.cs
+++ src/Api/PubnubApi/EventEngine/Core/Engine.cs
@@ -2,10 +2,31 @@
 
 namespace PubnubApi.PubnubEventEngine.Core {
 	public abstract class Engine {
-		private EffectDispatcher dispatcher;
+		public EventQueue eventQueue = new EventQueue();
+		
+		private EffectDispatcher dispatcher = new EffectDispatcher();
 		private State currentState;
+
+		public Engine() {
+			eventQueue.onEventQueued += OnEvent;
+		}
+
+		~Engine() {
+			eventQueue.onEventQueued -= OnEvent;
+		}
+
+		private async void OnEvent() {
+			currentState = await Transition(eventQueue.Dequeue());
+		}
+
+		/// <summary>
+		/// Implement event handling here.
+		/// </summary>
 		public abstract Task<State> Transition(Event e);
 
+		/// <summary>
+		/// Launch the invocations associated with transitioning between states
+		/// </summary>
 		private async Task ExecuteStateChange(State s1, State s2, params IEffectInvocation[] invocations) {
 			foreach (var effectInvocation in s1.onExit) {
 				await dispatcher.Dispatch(effectInvocation);
---
+++ src/Api/PubnubApi/EventEngine/Core/EventQueue.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PubnubApi.PubnubEventEngine.Core {
+	public class EventQueue {
+		private volatile Queue<Event> eventQueue = new Queue<Event>();
+		private object lockObj = new object();
+
+		public event System.Action onEventQueued;
+
+		public void Enqueue(Event e) {
+			lock (lockObj) {
+				// TODO de-dupe? Throttle?
+				eventQueue.Enqueue(e);
+				onEventQueued?.Invoke();
+			}
+		}
+
+		public Event Dequeue() {
+			lock (lockObj) {
+				return eventQueue.Any() ? eventQueue.Dequeue() : null;
+			}
+		}
+	}
+}
