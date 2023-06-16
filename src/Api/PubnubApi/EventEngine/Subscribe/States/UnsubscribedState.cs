eb96107c96f6cf01875576b3f58af2e482bf5a49
Michał Dobrzański <michal.dobrzanski@pubnub.com>
2023-06-16 10:36:51 +0200

    WIP forgot some files - example

--- src/Api/PubnubApi/EventEngine/Subscribe/States/UnsubscribedState.cs
+++ src/Api/PubnubApi/EventEngine/Subscribe/States/UnsubscribedState.cs
@@ -1,13 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using PubnubApi.PubnubEventEngine.Core;
+using PubnubApi.PubnubEventEngine.Subscribe.Invocations;
 
 namespace PubnubApi.PubnubEventEngine.Subscribe.States {
-	internal class UnsubscribedState : Core.State {
+	internal class UnsubscribedState : SubscribeCommonState {
 		public override IEnumerable<IEffectInvocation> onEntry { get; }
 		public override IEnumerable<IEffectInvocation> onExit { get; }
-		public override Tuple<Core.State, IEnumerable<IEffectInvocation>> Transition(Event e) {
-			throw new NotImplementedException();
+		public override Tuple<Core.State, IEnumerable<IEffectInvocation>> Transition(Core.IEvent e) {
+			switch (e) {
+				case Events.SubscriptionChangedEvent subscriptionChanged:
+					return new Tuple<Core.State, IEnumerable<IEffectInvocation>>(
+						new HandshakingState() {
+							channels = subscriptionChanged.channels,
+							channelGroups = subscriptionChanged.channelGroups,
+							cursor = cursor
+						},
+						new[] {
+							new HandshakeInvocation() {
+								channels = subscriptionChanged.channels,
+								channelGroups = subscriptionChanged.channelGroups,
+								cursor = cursor
+							}
+						}
+					);
+											
+					default: return null;
+			}
 		}
 	}
 }
