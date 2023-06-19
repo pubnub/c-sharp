6963d442f6933a020f89d937bbef5b7da00d2ddd
Michał Dobrzański <michal.dobrzanski@pubnub.com>
2023-06-19 12:21:30 +0200

    WIP Events

--- src/Api/PubnubApi/EventEngine/Subscribe/Events/SubscriptionEvents.cs
+++ src/Api/PubnubApi/EventEngine/Subscribe/Events/SubscriptionEvents.cs
@@ -12,4 +12,47 @@ namespace PubnubApi.PubnubEventEngine.Subscribe.Events {
 		public IEnumerable<string> channelGroups;
 		public SubscriptionCursor cursor;
 	}
+
+	public class HandshakeSuccessEvent : Core.IEvent {
+		public SubscriptionCursor cursor;
+	}
+
+	public class HandshakeFailureEvent : Core.IEvent {
+		// TODO status or reason?
+		public PNStatus status;
+	}
+
+	public class HandshakeReconnectSuccessEvent : Core.IEvent {
+		public SubscriptionCursor cursor;
+	}
+
+	public class HandshakeReconnectFailureEvent : Core.IEvent {
+		// TODO status or reason?
+		public PNStatus status;
+	}
+
+	public class HandshakeReconnectRetryEvent : Core.IEvent { }
+
+	public class HandshakeReconnectGiveUpEvent : Core.IEvent {
+		// TODO status or reason?
+		public PNStatus status;
+	}
+
+	public class ReceiveSuccessEvent : Core.IEvent {
+		public List<PNMessageResult<object>> messages;
+		public SubscriptionCursor cursor;
+	}
+
+	public class ReceiveFailureEvent : Core.IEvent {
+		// TODO status or reason?
+		public PNStatus status;
+	}
+
+	public class ReceiveReconnectSuccessEvent : ReceiveSuccessEvent { }
+	public class ReceiveReconnectFailure : ReceiveFailureEvent { }
+
+	public class ReceiveReconnectGiveUpEvent : Core.IEvent {
+		// TODO status or reason?
+		public PNStatus status;
+	}
 }
