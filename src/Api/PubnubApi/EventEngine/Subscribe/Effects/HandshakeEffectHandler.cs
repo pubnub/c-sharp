44674ced2a2236806e5ba3e8ea60c9665cae68d7
Michał Dobrzański <michal.dobrzanski@pubnub.com>
2023-07-10 13:34:46 +0200

    feat: handshake effect handler

--- src/Api/PubnubApi/EventEngine/Subscribe/Effects/HandshakeEffectHandler.cs
+++ src/Api/PubnubApi/EventEngine/Subscribe/Effects/HandshakeEffectHandler.cs
@@ -6,43 +6,65 @@ using PubnubApi.EndPoint;
 using PubnubApi.PubnubEventEngine.Core;
 using PubnubApi.PubnubEventEngine.Subscribe.Invocations;
 
-namespace PubnubApi.PubnubEventEngine.Subscribe.Effects {
-	internal class HandshakeEffectHandler : Core.IEffectHandler<HandshakeInvocation> {
-		private SubscribeManager2 manager;
-		private EventQueue eventQueue;
+namespace PubnubApi.PubnubEventEngine.Subscribe.Effects
+{
+    internal class HandshakeEffectHandler : Core.IEffectHandler<HandshakeInvocation>
+    {
+        private SubscribeManager2 manager;
+        private EventQueue eventQueue;
 
-		public HandshakeEffectHandler(SubscribeManager2 manager, EventQueue eventQueue) {
-			this.manager = manager;
-			this.eventQueue = eventQueue;
-		}
+        public HandshakeEffectHandler(SubscribeManager2 manager, EventQueue eventQueue)
+        {
+            this.manager = manager;
+            this.eventQueue = eventQueue;
+        }
 
-		public async Task Run(HandshakeInvocation invocation) {
-			// TODO fix this, probably wrong :)
-			var resp = await manager.HandshakeRequest<string>(
-				PNOperationType.PNSubscribeOperation,
-				invocation.Channels.ToArray(),
-				invocation.ChannelGroups.ToArray(),
-				null,
-				null,
-				invocation.InitialSubscribeQueryParams,
-				invocation.ExternalQueryParams
-			);
+        public async Task Run(HandshakeInvocation invocation)
+        {
+            var resp = await manager.HandshakeRequest<string>(
+                PNOperationType.PNSubscribeOperation,
+                invocation.Channels.ToArray(),
+                invocation.ChannelGroups.ToArray(),
+                null,
+                null,
+                invocation.InitialSubscribeQueryParams,
+                invocation.ExternalQueryParams
+            );
 
-			if (!resp.Item2.Error) {
-				// TODO move deserialization outside
-				// TODO does this need more error checking?
-				var handshakeResponse = JsonConvert.DeserializeObject<HandshakeResponse>(resp.Item1);
-				var c = new SubscriptionCursor() {
-					Region = handshakeResponse.Timetoken.Region,
-					Timetoken = handshakeResponse.Timetoken.Timestamp
-				};
-				
-				eventQueue.Enqueue(new Events.HandshakeSuccessEvent() {Cursor = c});
-			}
-		}
+            if (resp.Item2 is null || resp.Item2.Error)
+            {
+                eventQueue.Enqueue(new Events.HandshakeFailureEvent() { Status = resp.Item2 });
+                return;
+            }
 
-		public async Task Cancel() {
-			manager.HandshakeRequestCancellation();
-		}
-	}
+            HandshakeResponse handshakeResponse = null;
+
+            try
+            {
+                // TODO move deserialization outside
+                handshakeResponse = JsonConvert.DeserializeObject<HandshakeResponse>(resp.Item1);
+                var c = new SubscriptionCursor()
+                {
+                    Region = handshakeResponse.Timetoken.Region,
+                    Timetoken = handshakeResponse.Timetoken.Timestamp
+                };
+
+                eventQueue.Enqueue(new Events.HandshakeSuccessEvent() { Cursor = c });
+            }
+            catch (System.Exception e)
+            {
+                eventQueue.Enqueue(new Events.HandshakeFailureEvent()
+                    {
+                        Status = new PNStatus(e, PNOperationType.PNSubscribeOperation,
+                            PNStatusCategory.PNUnknownCategory, invocation.Channels, invocation.ChannelGroups)
+                    }
+                );
+            }
+        }
+
+        public async Task Cancel()
+        {
+            manager.HandshakeRequestCancellation();
+        }
+    }
 }
