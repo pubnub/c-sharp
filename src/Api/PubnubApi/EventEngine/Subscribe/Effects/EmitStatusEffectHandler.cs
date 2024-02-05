b2233492d62d9bd7502640a4c37ec778a3b8e14d
Xavrax <matedahlke@gmail.com>
2024-02-05 19:39:34 +0100

    wip for ci

--- src/Api/PubnubApi/EventEngine/Subscribe/Effects/EmitStatusEffectHandler.cs
+++ src/Api/PubnubApi/EventEngine/Subscribe/Effects/EmitStatusEffectHandler.cs
@@ -22,6 +22,15 @@ namespace PubnubApi.EventEngine.Subscribe.Effects
 
 		public override async Task Run(EmitStatusInvocation invocation)
 		{
+            if (invocation.Status == null) 
+                throw new Exception("Status is null");
+
+            if (this.statusEmitterFunction == null)
+                throw new Exception("StatusEmitterFunction is null");
+
+            if (this.pubnubInstance == null)
+                throw new Exception("PubnubInstance is null");
+
 			this.statusEmitterFunction(this.pubnubInstance, invocation.Status);
 		}
 	}
