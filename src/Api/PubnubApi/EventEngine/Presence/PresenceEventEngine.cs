b81c4977e8c53bd4814d540f8c75463766f28536
Mohit Tejani <mohit.tejani@pubnub.com>
2024-03-11 12:23:06 +0530

    PresenceEventEngine and its factory class implementation

---
+++ src/Api/PubnubApi/EventEngine/Presence/PresenceEventEngine.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using PubnubApi.EndPoint;
+using PubnubApi.EventEngine.Core;
+namespace PubnubApi.EventEngine.Presence
+{
+	public class PresenceEventEngine : Engine
+	{
+		public PresenceEventEngine(PNConfiguration pnConfiguration, HeartbeatOperation heartbeatOperation, LeaveOperation leaveOperation)
+		{
+			var heartbeatEffectHandler = new Effects.HeartbeatEffectHandler(heartbeatOperation, eventQueue);
+			dispatcher.Register<Invocations.HeartbeatInvocation, Effects.HeartbeatEffectHandler>(heartbeatEffectHandler);
+
+			var delayedHeartbeatEffectHandler = new Effects.DelayedHeartbeatEffectHandler(pnConfiguration, heartbeatOperation, eventQueue);
+			dispatcher.Register<Invocations.DelayedHeartbeatInvocation, Effects.DelayedHeartbeatEffectHandler>(delayedHeartbeatEffectHandler);
+			dispatcher.Register<Invocations.CancelDelayedHeartbeatInvocation, Effects.DelayedHeartbeatEffectHandler>(delayedHeartbeatEffectHandler);
+
+			var waitEffectHandler = new Effects.WaitEffectHandler(eventQueue);
+			dispatcher.Register<Invocations.WaitInvocation, Effects.WaitEffectHandler>(waitEffectHandler);
+			dispatcher.Register<Invocations.CancelWaitInvocation, Effects.WaitEffectHandler>(waitEffectHandler);
+
+			var leaveEffectHandler = new Effects.LeaveEffectHandler(leaveOperation);
+			dispatcher.Register<Invocations.LeaveInvocation, Effects.LeaveEffectHandler>(leaveEffectHandler);
+
+			currentState = new States.InactiveState();
+		}
+	}
+}
+
