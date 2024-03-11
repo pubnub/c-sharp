279dcf4ebcb8a2ac58dc2488c5f392cbb9773a8b
Mohit Tejani <mohit.tejani@pubnub.com>
2024-03-11 12:23:25 +0530

    PresenceOperation class implementation

---
+++ src/Api/PubnubApi/EndPoint/Presence/PresenceOperation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using PubnubApi.EventEngine.Presence;
+using PubnubApi.EventEngine.Presence.Events;
+
+namespace PubnubApi.EndPoint.Presence
+{
+	public class PresenceOperation<T>
+	{
+		private PresenceEventEngineFactory presenceEventEngineFactory;
+		private PresenceEventEngine presenceEventEngine;
+		public PresenceOperation(Pubnub instance, string instanceId, IPubnubLog pubnubLog, TelemetryManager telemetryManager, TokenManager tokenManager, PresenceEventEngineFactory presenceEventEngineFactory)
+		{
+			this.presenceEventEngineFactory = presenceEventEngineFactory;
+			if (this.presenceEventEngineFactory.hasEventEngine(instanceId)) {
+				presenceEventEngine = this.presenceEventEngineFactory.getEventEngine(instanceId);
+			} else {
+				presenceEventEngine = this.presenceEventEngineFactory.initializeEventEngine<T>(instanceId, instance, pubnubLog, telemetryManager, tokenManager);
+			}
+		}
+
+		public void Start(string[] channels, string[] channelGroups)
+		{
+			this.presenceEventEngine.eventQueue.Enqueue(new JoinedEvent() {
+				Input = new EventEngine.Presence.Common.PresenceInput() { Channels = channels, ChannelGroups = channelGroups }
+			});
+		}
+	}
+}
+
