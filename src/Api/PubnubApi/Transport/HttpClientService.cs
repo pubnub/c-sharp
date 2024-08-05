082829e43e638a3c8321554e7f64910cb69f01cf
Mohit Tejani <mohit.tejani@pubnub.com>
2024-08-05 13:22:17 +0530

    * Target framework upgrade * Initial code for HttpClient

---
+++ src/Api/PubnubApi/Transport/HttpClientService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PubnubApi
+{
+	public class HttpClientService
+	{
+		private static readonly HttpClient _httpClient = new HttpClient();
+
+		public HttpClientService()
+		{
+		}
+
+		public async Task<HttpResponseMessage> GetRequest(string url, CancellationToken cancellationToken, TimeSpan? timeout = null)
+		{
+			if (timeout.HasValue) _httpClient.Timeout = (TimeSpan)timeout;
+			return await _httpClient.GetAsync(url, cancellationToken);
+		}
+
+		public Task<HttpResponseMessage> PostRequest(string url, HttpContent content, CancellationToken cancellationToken)
+		{
+			return _httpClient.PostAsync(url, content, cancellationToken);
+		}
+
+		public Task<HttpResponseMessage> PutRequest(string url, HttpContent content, CancellationToken cancellationToken)
+		{
+			return _httpClient.PutAsync(url, content, cancellationToken);
+		}
+
+		public Task<HttpResponseMessage> DeleteRequest(string url, CancellationToken cancellationToken)
+		{
+			return _httpClient.DeleteAsync(url);
+		}
+
+	}
+}
+
---
+++ src/Api/PubnubApi/Transport/IHttpClientService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace PubnubApi.Transport
+{
+	public interface IHttpClientService <TRequest, TResponse>
+	{
+		Task<TResponse> GetRequest(TRequest request);
+
+		Task<TResponse> PostRequest(TRequest request);
+
+		Task<TResponse> PutRequest(TRequest request);
+
+		Task<TResponse> DeleteRequest(TRequest request);
+	}
+}
+
