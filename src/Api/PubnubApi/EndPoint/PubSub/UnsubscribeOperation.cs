74a09a960c2fef3fa154338680e9aca4fef14bcf
PUBNUB\jakub.grzesiowski <jakub.grzesiowski@pubnub.com>
2024-09-26 13:28:42 +0200

    Fix build issues for Unity and UWP projects

--- src/Api/PubnubApi/EndPoint/PubSub/UnsubscribeOperation.cs
+++ src/Api/PubnubApi/EndPoint/PubSub/UnsubscribeOperation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using PubnubApi.Interface;
 
 namespace PubnubApi.EndPoint
